65edcb2d02dd1d0723c07a432af512120f5068f6
Peter Marsh <p.marsh@mac.com>
2018-03-16 17:06:58 -0400

    mines work and board info is in array

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
 const gameBoard = document.getElementById("game-board");
+const typeOfCell = ["cell", "empty", "bomb"];
+const bombArray = [];
+const boardArray = [];
 //
-function random(min, max) {
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-};
 //
 function makeBoard(width, height, minesQty) {
-    for (let y = 0; y < height; y++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        for (let x = 0; x < width; x++) {
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.setAttribute("id", (x + 1));
-            row.appendChild(cell);
-            cell.addEventListener("click", function (event) {
-                console.log(event.target.id);
-            });
+    this.width = width;
+    this.height = height;
+    this.minesQty = minesQty;
+    this.totalCells = this.width * this.height;
+    document.getElementById("mines-left").innerHTML = this.minesQty;
+    let tileNumber = 0;
+
+    for (i = 0; i < minesQty; i++) {
+        var randomNum = Math.floor(Math.random() * (this.totalCells + 1));
+        if (!bombArray.includes(randomNum) && randomNum !== 0) {
+            bombArray.push(randomNum);
+        } else {
+            i--
         }
-        gameBoard.appendChild(row);
     }
-};
+    console.log(bombArray);
+
+    this.drawBoard = function () {
+        for (let y = 0; y < height; y++) {
+            let row = document.createElement("div");
+            boardArray.push([]);
+            row.classList.add("row");
+            for (let x = 0; x < width; x++) {
+                let tile = document.createElement("div");
+                tile.classList.add("cell");
+                tileNumber = tileNumber + 1
+                tile.dataset.tileNum = tileNumber;
+                row.appendChild(tile);
+                boardArray[y].push(tileNumber);
+                tile.addEventListener("click", function eventHandler(event) {
+                    let cell = event.target;
+                    if (bombArray.includes(Number(cell.dataset.tileNum))) {
+                        tile.classList.add("mine")
+                    }
+
+                });
+            }
+            gameBoard.appendChild(row);
+        }
+
+    };
+
+}
 //
 function timer() {
     var start = new Date().getTime();
@@ -34,6 +61,7 @@ function timer() {
         document.getElementById("time-left").innerHTML = elapsed;
     }, 100);
 };
+
 //
-var board = new makeBoard(8, 8, 0);
-timer();
+var board = new makeBoard(8, 8, 10);
+board.drawBoard();
