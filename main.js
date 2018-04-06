88e42ea4b541a0ed347663c0249b3eb062362394
Peter Marsh <p.marsh@mac.com>
2018-04-06 12:44:08 -0400

    updated width height mines function

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const gameBoard = document.getElementById("game-board");
-const bombArray = [];
-const boardArray = [];
-// let fwidth;
-// let fheight;
-// let fmines;
-// function getInfo() {
-//     fwidth = document.forms.myForm.fwidth.value;
-//     fheight = document.forms.myForm.fheight.value;
-//     fmines = document.forms.myForm.fmines.value;
-//     return fwidth, fheight, fmines;
-// }
-// getInfo();
+const foo = document.querySelectorAll("div.cell");
+//
+let fwidth = document.getElementById("fwidth");
+let fheight = document.getElementById("fheight");
+let fmines = document.getElementById("fmines");
+var board = new Board(fwidth.value, fheight.value, fmines.value);
+fwidth.addEventListener("change", board.changeWidth)
+fheight.addEventListener("change", board.changeHeight)
+fmines.addEventListener("change", board.changeMines)
+// board.drawBoard();
 
 // constructor function for board
 function Board(width, height, mines) {
+    this.bombArray = [];
     let click = 0;
     this.width = width;
     this.height = height;
@@ -21,48 +20,60 @@ function Board(width, height, mines) {
     this.totalCells = this.width * this.height;
     document.getElementById("mines-left").innerHTML = this.minesQty;
     let tileNumber = 0;
-    // creates an array of random locations for bombs, pushes to bombArray for reference
-    for (i = 0; i < this.minesQty; i++) {
-        var randomNum = Math.round(Math.random() * (this.totalCells + 1));
-        if (!bombArray.includes(randomNum) && randomNum !== 0) {
-            bombArray.push(randomNum);
-        } else {
-            i--
-        }
-    };
+    // creates an array of random locations for bombs, pushes to this.bombArray for reference
     //
-    console.log(bombArray);
+    
     // Draws the board
-    this.drawBoard = function () {
-        for (let y = 0; y < height; y++) {
+    this.placeMines = () => {
+        this.bombArray = [];
+        for (i = 0; i <= this.minesQty; i++) {
+            var randomNum = Math.round(Math.random() * (this.totalCells + 1));
+            if (!this.bombArray.includes(randomNum) && randomNum !== 0) {
+                this.bombArray.push(randomNum);
+            } else {
+                i--
+            }
+        };
+        
+    }
+    this.drawBoard = () => {
+        this.placeMines();
+        if (gameBoard.firstChild) {
+            while (gameBoard.firstChild) {
+                gameBoard.removeChild(gameBoard.firstChild);
+            }
+        };
+        for (let y = 0; y < this.height; y++) {
             let row = document.createElement("div");
-            boardArray.push([]);
+
             row.classList.add("row");
-            for (let x = 0; x < width; x++) {
+            for (let x = 0; x < this.width; x++) {
                 let tile = document.createElement("div");
                 tile.classList.add("cell");
                 tileNumber = tileNumber + 1;
                 tile.dataset.tileNumber = tileNumber;
+                if (this.bombArray.includes(Number(tile.dataset.tileNumber))) {
+                    tile.dataset.minestatus = "mine"
+                };
                 tile.dataset.row = y;
                 tile.dataset.col = x;
                 row.appendChild(tile);
-                boardArray[y].push(tileNumber);
+
                 // adds event listeners
                 tile.addEventListener("click", this.handleClickLeft);
                 tile.addEventListener("contextmenu", this.handleClickRight, false);
             }
             gameBoard.appendChild(row);
         }
-
     };
     // Left Click
-    this.handleClickLeft = function () {
+    this.handleClickLeft = () => {
         let cell = event.target;
         let typeOfCell = cell.className;
         console.log(event.target.dataset);
         console.log(typeOfCell);
-        //adds mine css class if in bombArray
-        if (bombArray.includes(Number(cell.dataset.tileNumber))) {
+        //adds mine css class if in this.bombArray
+        if (this.bombArray.includes(Number(cell.dataset.tileNumber))) {
             cell.classList.add("mine");
             //alerts you if you hit a mine
             if (cell.className === "cell mine") {
@@ -70,42 +81,48 @@ function Board(width, height, mines) {
                     alert("I'm afraid your luck has run out.");
                 }, 500);
                 // location.reload();
+                board.loser();
             }
         };
-        //adds empty class if not in bombArray
-        if (!bombArray.includes(Number(cell.dataset.tileNumber))) {
+        //adds empty class if not in this.bombArray
+        if (!this.bombArray.includes(Number(cell.dataset.tileNumber))) {
             cell.classList.add("empty");
             click++;
             board.timer();
-            console.log(click);
-        }
+        };
     };
     // Right Click
-    this.handleClickRight = function (event) {
+    this.handleClickRight = (event) => {
         event.preventDefault();
         if (event.target.className === "cell") {
             if (board.minesQty > 0) {
-            event.target.classList.add("flag");
-            document.getElementById("mines-left").innerHTML = board.minesQty -= 1
-        }
+                event.target.classList.add("flag");
+                document.getElementById("mines-left").innerHTML = board.minesQty -= 1
+            }
         } else if (event.target.className === "cell flag") {
             event.target.classList.remove("flag");
             document.getElementById("mines-left").innerHTML = board.minesQty += 1
         }
         return false;
     };
-    // this.checkNeighbors = function () {
-    //     var queue = [];
-    //     rowNum = Number(event.target.dataset.row);
-    //     colNum = Number(event.target.dataset.col);
-    //     queue.push([rowNum, colNum]);
-    //     console.log(queue);
-    //     while (queue.length) {
-    //         queue.shift();
-    //     };
-    // };
+    this.changeWidth = (event) => {
+        this.width = event.target.value;
+    };
+    this.changeHeight = (event) => {
+        this.height = event.target.value;
+    }
+    this.changeMines = (event) => {
+        this.minesQty = event.target.value;
+    }
+    // loss function 
+    this.loser = function () {
+        for (let i = 0; i < foo.length; i++) {
+
+        }
+    };
+
     // timer function
-    this.timer = function () {
+    this.timer = () => {
         if (click === 1) {
             var start = new Date().getTime();
             var elapsed = '0.0';
@@ -120,7 +137,4 @@ function Board(width, height, mines) {
         }
     }
 };
-
-// 
-var board = new Board(8, 8, 10);
-board.drawBoard();
+//
